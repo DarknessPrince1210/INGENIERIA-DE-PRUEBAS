aa9261f6cdd5f309ba7285b12ee829ac22ebf131
DarknessPrince1210 <diaz.tiburcio.ismael@gmail.com>
2022-08-26 09:50:09 -0500

    Factorial

--- TAREA1/tabsMultiplicar.js
+++ TAREA1/tabsMultiplicar.js
@@ -1,8 +1,10 @@
 document.write('<BR>'+'<b>TABLAS DEL 0-10</b>'+'<BR>'+'<BR>')
 for(i=0;i<=10;i++){
+    document.write(`Tabla del ${i}<br><br>`)
     for(a=1;a<=10;a++){
         num=i*a;
-        document.write(num+'<br>');
+        document.write(`${i}X${a}: ${num}<br>`);
+        // document.write(i+'X'+a+': '+num+'<br>');
     }
     document.write('<br>')
 
